3d9c0bff48c68bc7d2f6e95e784e4a6ff086e3c8
asare George <asareg365@gmail.com>
2026-01-29 22:01:53 +0000

    furnish me with firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyBhS1B5Mx_onOBf3Imup4MyOMuxk-qJj1o",
-  authDomain: "studio-1004537855-178e0.firebaseapp.com",
-  projectId: "studio-1004537855-178e0",
-  storageBucket: "studio-1004537855-178e0.appspot.com",
-  messagingSenderId: "38469851703",
-  appId: "1:38469851703:web:8a64c5092781668743bdb2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
