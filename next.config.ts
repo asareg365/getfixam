1dbd552ac2d89a594d5e59c96ac634d25675c711
asare George <asareg365@gmail.com>
2026-01-30 22:29:11 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: ["https://6000-firebase-studio-1769561211096.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev"],
   experimental: {
-    allowedDevOrigins: ["https://6000-firebase-studio-1769561211096.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev"],
   },
   /* config options here */
   typescript: {
