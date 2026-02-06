81f34522a1db7c0a95b186b6400cd76d2b3a97e0
asare George <asareg365@gmail.com>
2026-02-06 19:02:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1004537855-178e0",
+  "appId": "1:38469851703:web:8a64c5092781668743bdb2",
+  "apiKey": "AIzaSyBhS1B5Mx_onOBf3Imup4MyOMuxk-qJj1o",
+  "authDomain": "studio-1004537855-178e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "38469851703"
+};
